2a459ba97d65158fa1966054fe0e46575e8cc87f
jpatag <jacob.patag04@gmail.com>
2025-09-30 23:12:04 -0400

    added analytics and changed favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import About from '@/components/About';
 import Portfolio from '@/components/Portfolio';
 import Accomplishments from '@/components/Accomplishments';
 import Contact from '@/components/Contact';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <main>
         <Hero />
